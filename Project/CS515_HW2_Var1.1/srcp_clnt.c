e77202694dd9415c03bf82d9ad159f2d644aff83
arvin <arvin.tcm@gmail.com>
2016-02-18 22:30:48 -0800

    test

--- Project/CS515_HW2_Var1.1/srcp_clnt.c
+++ Project/CS515_HW2_Var1.1/srcp_clnt.c
@@ -20,6 +20,10 @@
 #include "srcp.h"
 
 #define DATA_BUF_SIZE 128
+static char ipBuff[BUF_SIZE + 10];
+static char filePathBuff[BUF_SIZE + 10];
+
+int parseReq(char *inBuf, char opCode);
 
 /*
  * 
@@ -28,13 +32,15 @@ int main(int argc, char** argv) {
     int sock_fd;
     int n;
     struct sockaddr_in serv_addr;
-    char *msg = "This is a test message for reg test";
-    char buffer[DATA_BUF_SIZE];
-
+    char buffer[BUF_SIZE];
+    
     if (argc != 2) {
         printf("%s [server_ip]:filepath\n", argv[0]);
         exit(1);
     }
+
+    /* parse the argument */
+    parseReq(argv[1], 'U');
 
     /* open a socket */
     if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -46,19 +52,20 @@ int main(int argc, char** argv) {
     bzero(&serv_addr, sizeof (serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(SRCP_SERV_PORT);
-    inet_pton(AF_INET, argv[1], &serv_addr.sin_addr);
+    inet_pton(AF_INET, ipBuff, &serv_addr.sin_addr);
 
     if (connect(sock_fd, (struct sockaddr *) &serv_addr, sizeof (serv_addr)) < 0) {
         perror("connect error");
         exit(1);
     }
 
+
     /* send the registration message to the server */
-    if (write(sock_fd, msg, strlen(msg)) < 0) {
+    if (write(sock_fd, filePathBuff, strlen(filePathBuff)) < 0) {
         perror("socket write failed\n");
         exit(1);
     }
-    
+
     /* wait for the reply from the server */
     n = read(sock_fd, buffer, DATA_BUF_SIZE);
     if (n < 0) {
@@ -72,3 +79,16 @@ int main(int argc, char** argv) {
     return (EXIT_SUCCESS);
 }
 
+int parseReq(char *inBuf, char opCode) {
+    char *p;
+    p = strchr(inBuf, ':');
+    if (p == NULL) {
+        printf("Invalid srcp file path");
+        exit(1);
+    }
+    *p = '\0';
+    strcpy(ipBuff, inBuf);
+    filePathBuff[0] = opCode;
+    strcpy(filePathBuff + 1, p + 1);
+    return SUCCESS;
+}
